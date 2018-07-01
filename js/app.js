d5481a5aa759b53a74932983e586be4c9063e344
Robert Cisek <r.cisek@o2.pl>
2018-07-01 20:44:39 +0200

    added json file and created controller responsible fetching data

--- js/app.js
+++ js/app.js
@@ -1,2 +1,16 @@
+'use strict';
 
-let app = angular.module('app', []);
+var app = angular.module( 'app' , [] );
+
+app.controller( 'products' , [ '$scope' , '$http' , function( $scope , $http ){
+
+    $http({
+        method: 'get',
+        url: 'model/produkty.json'
+    }).then(function (response) {
+        $scope.products = response.data;
+    },function (error){
+        console.log(error, 'can not get data.');
+    });
+
+}]);
